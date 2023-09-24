1c220941be9d5dbffd761a13631847a7ce122310
AndiMahardika <andi.mahardika.mulya@gmail.com>
2023-09-24 10:07:36 -0700

    module exports 1

--- CoffeeMachine/index.js
+++ CoffeeMachine/index.js
@@ -1,7 +1,19 @@
-console.log("Menyalakan mesin kopi");
-console.log("Menggiling biji kopi");
-console.log("Memanaskan air");
-console.log("Mencampurkan air dan kopi");
-console.log("Menuangkan kopi ke dalam gelas");
-console.log("Menuangkan susu ke dalam gelas");
-console.log("Kopi Anda sudah siap!");
+// console.log("Menyalakan mesin kopi");
+// console.log("Menggiling biji kopi");
+// console.log("Memanaskan air");
+// console.log("Mencampurkan air dan kopi");
+// console.log("Menuangkan kopi ke dalam gelas");
+// console.log("Menuangkan susu ke dalam gelas");
+// console.log("Kopi Anda sudah siap!");
+
+const coffeeStock = require(`./state`);
+
+const makeCoffee = (type, miligrams) => {
+  if (coffeeStock[type] >= miligrams) {
+    console.log("Kopi berhasil dibuat!");
+  } else {
+    console.log("Biji kopi habis!");
+  }
+}
+ 
+makeCoffee("robusta", 80);
